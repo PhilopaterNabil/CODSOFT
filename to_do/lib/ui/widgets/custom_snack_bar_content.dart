05e0f2fa2adfefb7967fe6c1786fa2fea2b77f4d
Philopater_Nabil <fylwbatyrnbylnasr@gmail.com>
2024-07-07 16:04:49 +0300

    Refactor CustomSnackBarContent widget for responsiveness
    - Updated CustomSnackBarContent widget to be more responsive by using SizeConfig
    - Adjusted the height and width of the container and SVG assets based on device orientation
    - Removed unnecessary Stack widget and simplified layout

--- to_do/lib/ui/widgets/custom_snack_bar_content.dart
+++ to_do/lib/ui/widgets/custom_snack_bar_content.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:to_do/ui/size_config.dart';
 import 'package:to_do/ui/theme.dart';
 
 class CustomSnackBarContent extends StatelessWidget {
@@ -12,73 +13,89 @@ class CustomSnackBarContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      clipBehavior: Clip.none,
-      children: [
-        Container(
-          padding: const EdgeInsets.all(16),
-          height: MediaQuery.of(context).size.height * 0.1,
-          decoration: BoxDecoration(
-            color: redErorrContainerClr,
-            borderRadius: BorderRadius.circular(20),
-          ),
-          child: Row(
-            children: [
-              SizedBox(width: MediaQuery.of(context).size.width * 0.05),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      errorText,
-                      style: titleStyle.copyWith(color: white),
-                      maxLines: 2,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                    const Spacer(),
-                    Text(message, style: subTitleStyle.copyWith(color: white)),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-        Positioned(
-          bottom: 0,
-          left: 0,
-          child: ClipRRect(
-            borderRadius: const BorderRadius.only(
-              bottomLeft: Radius.circular(20),
+    return Center(
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(16),
+            height: SizeConfig.orientation == Orientation.landscape
+                ? MediaQuery.sizeOf(context).height * 0.23
+                : MediaQuery.sizeOf(context).height * 0.1,
+            width: SizeConfig.orientation == Orientation.landscape
+                ? MediaQuery.sizeOf(context).width * 0.5
+                : MediaQuery.sizeOf(context).width,
+            decoration: BoxDecoration(
+              color: redErorrContainerClr,
+              borderRadius: BorderRadius.circular(20),
             ),
-            child: SvgPicture.asset(
-              'assets/icons/bubbles.svg',
-              height: MediaQuery.of(context).size.height * 0.07,
-              width: MediaQuery.of(context).size.width * 0.07,
-              color: redErorrbubblesClr,
+            child: Row(
+              children: [
+                SizedBox(width: MediaQuery.sizeOf(context).width * 0.05),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        errorText,
+                        style: titleStyle.copyWith(color: white),
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      const Spacer(),
+                      Text(message,
+                          style: subTitleStyle.copyWith(color: white)),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ),
-        ),
-        Positioned(
-          top: -16,
-          left: 2,
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              SvgPicture.asset(
-                'assets/icons/fail.svg',
-                height: MediaQuery.of(context).size.height * 0.046,
+          Positioned(
+            bottom: 0,
+            left: 0,
+            child: ClipRRect(
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(20),
               ),
-              Positioned(
-                top: 10,
-                child: SvgPicture.asset(
-                  'assets/icons/close.svg',
-                  height: MediaQuery.of(context).size.height * 0.02,
-                ),
+              child: SvgPicture.asset(
+                'assets/icons/bubbles.svg',
+                height: SizeConfig.orientation == Orientation.landscape
+                    ? MediaQuery.sizeOf(context).height * 0.16
+                    : MediaQuery.sizeOf(context).height * 0.07,
+                width: SizeConfig.orientation == Orientation.landscape
+                    ? MediaQuery.sizeOf(context).width * 0.17
+                    : MediaQuery.sizeOf(context).width * 0.07,
+                color: redErorrbubblesClr,
               ),
-            ],
+            ),
           ),
-        ),
-      ],
+          Positioned(
+            top: -16,
+            left: 2,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                SvgPicture.asset(
+                  'assets/icons/fail.svg',
+                  height: SizeConfig.orientation == Orientation.landscape
+                      ? MediaQuery.sizeOf(context).height * 0.1
+                      : MediaQuery.sizeOf(context).height * 0.046,
+                ),
+                Positioned(
+                  top: 10,
+                  child: SvgPicture.asset(
+                    'assets/icons/close.svg',
+                    height: SizeConfig.orientation == Orientation.landscape
+                        ? MediaQuery.sizeOf(context).height * 0.043
+                        : MediaQuery.sizeOf(context).height * 0.02,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
